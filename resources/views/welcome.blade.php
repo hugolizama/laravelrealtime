9981a4c8b40175064041c5aa54a65a9bf1c97b4b
Hugo Lizama Mejía <hugo.lizama@asamblea.gob.sv>
2024-02-20 15:57:47 -0600

    primer commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+	<div class="row justify-content-center">
+		<div class="col-md-8">
+			<div class="card">
+				<div class="card-header">Inicio</div>
+
+				<div class="card-body">					
+					Esta es una aplicaci&oacute;n en tiempo real.
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
